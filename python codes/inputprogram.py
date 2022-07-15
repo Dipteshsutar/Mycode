b28aeb97f1e424af3c42f7877539d2219cc125f8
Diptesh <108788659+Dipteshsutar@users.noreply.github.com>
2022-07-16 02:06:31 +0530

    Add files via upload

---
+++ python codes/inputprogram.py
@@ -0,0 +1,5 @@
+firstname = input("Enter your first name :")
+middlename = input("Enter your middle name :")
+lastname = input("Enter your last name :")
+
+print("Your full name is ",firstname,middlename,lastname)
